93b9abba86c2cab7c9af384c46e320f3b1d28e46
Desloratadine <2473609575@qq.com>
2026-05-15 20:39:32 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+// 游戏配置文件
+window.GAME_CONFIG = {
+    "gridSize": 5,
+    "totalLayers": 4,
+    "backpackCapacity": 6,
+    "flowersToLightBase": 3,
+    "maxLitTiles": 3,
+    "monsterInterval": 2,
+    "initialFlowers": 6,
+    "newTurfFlowersMinBase": 3,
+    "newTurfFlowersMaxBase": 6,
+    "cellBaseSize": 80,
+    "cellGap": 5,
+    "canvasPadding": 30,
+    "monsterDemandPercent": 0.5,
+    "layerFlowerRanges": [
+        {"min": 3, "max": 6},
+        {"min": 4, "max": 7},
+        {"min": 5, "max": 8},
+        {"min": 6, "max": 9}
+    ],
+    // 武器系统配置
+    "weaponUpgradeCosts": [0, 3, 4, 5],
+    "weaponDemandPercents": [0.5, 0.4, 0.3, 0.2, 0]
+};

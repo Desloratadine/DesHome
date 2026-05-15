02f0d3256e5ea55c1a9e9a55a571a127d9cd57b0
Desloratadine <2473609575@qq.com>
2026-05-15 13:49:28 +0800

    Add files via upload

---
+++ FlowerPicker/config.js
@@ -0,0 +1,23 @@
+// 游戏配置文件
+window.GAME_CONFIG = {
+    "gridSize": 5,
+    "totalLayers": 4,
+    "backpackCapacity": 6,
+    "flowersToLightBase": 3,
+    "maxLitTiles": 6,
+    "monsterInterval": 2,
+    "compassCost": 2,
+    "initialFlowers": 6,
+    "newTurfFlowersMinBase": 3,
+    "newTurfFlowersMaxBase": 6,
+    "cellBaseSize": 80,
+    "cellGap": 5,
+    "canvasPadding": 30,
+    "monsterDemandPercent": 0.5,
+    "layerFlowerRanges": [
+        {"min": 3, "max": 6},
+        {"min": 4, "max": 7},
+        {"min": 5, "max": 8},
+        {"min": 6, "max": 9}
+    ]
+};
